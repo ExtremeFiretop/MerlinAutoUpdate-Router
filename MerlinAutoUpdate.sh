031c4f7a73c26f339928f8f23af88c225c7f9ca2
Joel Samson <joel.samson@hotmail.com>
2023-10-03 13:24:22 -0400

    Update MerlinAutoUpdate.sh

--- MerlinAutoUpdate.sh
+++ MerlinAutoUpdate.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 URL_BASE="https://sourceforge.net/projects/asuswrt-merlin/files"
-URL_BETA_SUFFIX="Beta"
 URL_RELEASE_SUFFIX="Release"
 
 MODEL=$(nvram get model)
-URL_BETA="${URL_BASE}/${MODEL}/${URL_BETA_SUFFIX}/"
 URL_RELEASE="${URL_BASE}/${MODEL}/${URL_RELEASE_SUFFIX}/"
 SETTINGS_DIR="/jffs/addons/MerlinAutoUpdate"
 SETTINGSFILE="$SETTINGS_DIR/custom_settings.txt"
@@ -67,140 +65,20 @@ get_current_firmware() {
 get_latest_firmware() {
     local url="$1"
     local page=$(curl -s "$url")
-  
+
     local links_and_versions=$(echo "$page" | grep -o 'href="[^"]*'"$MODEL"'[^"]*\.zip' | sed 's/amp;//g; s/href="//' | 
-        awk -F'[_\.]' '{print $2"."$3"."$4" "$0}' | sort -t. -k1,1n -k2,2n -k3,3n)
-    
+        awk -F'[_\.]' '{print $3"."$4"."$5" "$0}' | sort -t. -k1,1n -k2,2n -k3,3n)
+
     local latest=$(echo "$links_and_versions" | tail -n 1)
-    local version=$(echo "$latest" | cut -d' ' -f1)
+    local version=$(echo "$latest" | cut -d' ' -f1 | awk -F. '{print $(NF-1)"."$NF}')
     local link=$(echo "$latest" | cut -d' ' -f2-)
-    
+
     # Extracting the correct link from the page
     local correct_link=$(echo "$link" | sed 's|^/|https://sourceforge.net/|')
-    
+
     echo "$version"
     echo "$correct_link"
 }
-
-# Embed functions from second script, modified as necessary.
-run_now() {
-
-Say "Running the task now...Checking for updates..."
-# Get current firmware version
-current_version=$(get_current_firmware)	
-	
-# Use set to read the output of the function into variables
-set -- $(get_latest_firmware "$URL_BETA")
-beta_version=$1
-beta_link=$2
-
-set -- $(get_latest_firmware "$URL_RELEASE")
-release_version=$1
-release_link=$2
-
-    # Compare versions before deciding to download
-    if [ "$beta_version" \> "$current_version" ]; then
-        Say "Latest beta version is $beta_version, downloading from $beta_link"
-        wget -O "${MODEL}_firmware.zip" "$beta_link"
-    elif [ "$release_version" \> "$current_version" ]; then
-        Say "Latest release version is $release_version, downloading from $release_link"
-        wget -O "${MODEL}_firmware.zip" "$release_link"
-    else
-        Say "Current firmware version $current_version is up to date."
-        if [[ $1 != "run_now" ]]; then  # Check if the first argument is not "cron"
-            read -p "Press Enter to return to the main menu..."
-        fi
-        return  # Exit the function early as there's no newer firmware
-    fi
-
-# Create directory for extracting firmware
-mkdir -p "/home/root/${MODEL}_firmware"
-
-# Extracting the firmware
-unzip -o "${MODEL}_firmware.zip" -d "/home/root/${MODEL}_firmware"
-
-# Define the path to the log file
-#log_file="/home/root/${MODEL}_firmware/Changelog-NG.txt"
-
-# Check if the log file exists
-#if [ ! -f "$log_file" ]; then
-#    echo "Log file does not exist at $log_file"
-#    exit 1
-#fi
-
-# Checking the log file for reset recommendation between two dates
-#log_contents=$(awk '/2023-09-30 00:00:00/,/2023-10-01 23:59:59/' "$log_file")
-
-#if echo "$log_contents" | grep -q "reset recommended"; then
-#    echo -e "Factory Default Reset is recommended according to the logs. Would you like to continue anyways?"
-#	read choice
-#	if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
-#       continue
-#    else
-#        exit
-#    fi
-#else
-#    echo "No reset is recommended according to the logs."
-#fi
-
-# Use Get_Custom_Setting to retrieve the previous choice
-previous_choice=$(Get_Custom_Setting "local" "n")
-
-# Logging user's choice
-# Check for the presence of "rog" in filenames in the extracted directory
-cd "/home/root/${MODEL}_firmware"
-rog_file=$(ls | grep -i '_rog_')
-pure_file=$(ls | grep -i '_pureubi.w' | grep -iv 'rog')
-
-if [ ! -f "$SETTINGSFILE" ]; then
-    # On the first run, when the settings file doesn't exist, prompt the user
-    if [ ! -z "$rog_file" ]; then
-        echo -e "\033[31mFound ROG build: $rog_file. Would you like to use the ROG build? (y/n)\033[0m"
-        
-        # Use the previous_choice as the default value
-        read -p "Enter your choice [$previous_choice]: " choice
-
-        # Use the entered choice or the default value if the input is empty
-        choice="${choice:-$previous_choice}"
-
-        if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
-            firmware_file="$rog_file"
-            Update_Custom_Settings "local" "y"
-        else
-            firmware_file="$pure_file"
-            Update_Custom_Settings "local" "n"
-        fi
-    else
-        firmware_file="$pure_file"
-        Update_Custom_Settings "local" "n"
-    fi
-else
-    # On subsequent runs, use the stored choice without prompting
-    if [ "$previous_choice" = "y" ]; then
-        firmware_file="$rog_file"
-    else
-        firmware_file="$pure_file"
-    fi
-fi
-
-# Flashing the chosen firmware
-echo -e "\033[32mFlashing $firmware_file...\033[0m"
-hnd-write "$firmware_file"  # Execute the command to flash the firmware.
-
-# Wait for 3 minutes
-sleep 60
-
-# Reboot the router
-reboot
-    
-    read -p "Press Enter to continue..."
-}
-
-if [[ $1 == "run_now" ]]; then
-    # If the argument is "run_now", call the run_now function and exit
-    run_now
-    exit 0
-fi
 
 change_build_type() {
     echo "Changing Build Type..."
@@ -210,25 +88,6 @@ change_build_type() {
 cd "/home/root/${MODEL}_firmware"
 rog_file=$(ls | grep -i '_rog_')
 pure_file=$(ls | grep -i '_pureubi.w' | grep -iv 'rog')
-
-# Function to get custom setting value from the settings file
-Get_Custom_Setting() {
-    local setting_type="$1"
-    local default_value="$2"
-
-    if [ -f "$SETTINGSFILE" ]; then
-        case "$setting_type" in
-            local)
-                grep -q "FirmwareVersion_setting" "$SETTINGSFILE" && grep "FirmwareVersion_setting" "$SETTINGSFILE" | cut -f2 -d' ' || echo "$default_value"
-                ;;
-            *)
-                echo "$default_value"
-                ;;
-        esac
-    else
-        echo "$default_value"
-    fi
-}
 
 # Use Get_Custom_Setting to retrieve the previous choice
 previous_choice=$(Get_Custom_Setting "local" "n")
@@ -325,7 +184,7 @@ change_schedule() {
     # Translate the current schedule to English
     current_schedule_english=$(translate_schedule "$current_schedule")
     
-    echo "Current Schedule: $current_schedule_english"
+    echo -e "\033[32mCurrent Schedule: $current_schedule_english \033[0m"
   else
     echo "Cron job 'MerlinAutoUpdate' does not exist. It will be added."
   fi
@@ -358,11 +217,127 @@ change_schedule() {
   
   # Display the updated schedule
   current_schedule_english=$(translate_schedule "$new_schedule")
-  echo "Updated Schedule: $current_schedule_english"
+  echo -e "\033[32mUpdated Schedule: $current_schedule_english \033[0m"
   
   # Return to the main menu
   read -p "Press Enter to return to the main menu..."
 }
+
+# Embed functions from second script, modified as necessary.
+run_now() {
+
+Say "Running the task now...Checking for updates..."
+# Get current firmware version
+#current_version=$(get_current_firmware)	
+current_version="388.3"
+
+# Use set to read the output of the function into variables
+set -- $(get_latest_firmware "$URL_RELEASE")
+release_version=$1
+release_link=$2
+
+    # Compare versions before deciding to download
+    if [ "$release_version" \> "$current_version" ]; then
+        Say "Latest release version is $release_version, downloading from $release_link"
+        wget -O "${MODEL}_firmware.zip" "$release_link"
+    else
+        Say "Current firmware version $current_version is up to date."
+        if [[ $1 != "run_now" ]]; then  # Check if the first argument is not "cron"
+            read -p "Press Enter to return to the main menu..."
+        fi
+        return  # Exit the function early as there's no newer firmware
+    fi
+
+# Create directory for extracting firmware
+mkdir -p "/home/root/${MODEL}_firmware"
+
+# Extracting the firmware
+unzip -o "${MODEL}_firmware.zip" -d "/home/root/${MODEL}_firmware"
+
+# Define the path to the log file
+#log_file="/home/root/${MODEL}_firmware/Changelog-NG.txt"
+
+# Check if the log file exists
+#if [ ! -f "$log_file" ]; then
+#    echo "Log file does not exist at $log_file"
+#    exit 1
+#fi
+
+# Checking the log file for reset recommendation between two dates
+#log_contents=$(awk '/2023-09-30 00:00:00/,/2023-10-01 23:59:59/' "$log_file")
+
+#if echo "$log_contents" | grep -q "reset recommended"; then
+#    echo -e "Factory Default Reset is recommended according to the logs. Would you like to continue anyways?"
+#	read choice
+#	if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
+#       continue
+#    else
+#        exit
+#    fi
+#else
+#    echo "No reset is recommended according to the logs."
+#fi
+
+# Use Get_Custom_Setting to retrieve the previous choice
+previous_choice=$(Get_Custom_Setting "local" "n")
+
+# Logging user's choice
+# Check for the presence of "rog" in filenames in the extracted directory
+cd "/home/root/${MODEL}_firmware"
+rog_file=$(ls | grep -i '_rog_')
+pure_file=$(ls | grep -i '_pureubi.w' | grep -iv 'rog')
+
+if [ ! -f "$SETTINGSFILE" ]; then
+    if [ ! -z "$rog_file" ]; then
+        # Check if the first argument is "run_now"
+        if [ "$1" == "run_now" ]; then
+            # If the argument is "run_now", default to the "Pure Build"
+            firmware_file="$pure_file"
+            Update_Custom_Settings "local" "n"
+        else
+            # Otherwise, prompt the user for their choice
+            echo -e "\033[31mFound ROG build: $rog_file. Would you like to use the ROG build? (y/n)\033[0m"
+            read -p "Enter your choice [$previous_choice]: " choice
+            choice="${choice:-$previous_choice}"
+            if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
+                firmware_file="$rog_file"
+                Update_Custom_Settings "local" "y"
+            else
+                firmware_file="$pure_file"
+                Update_Custom_Settings "local" "n"
+            fi
+        fi
+    else
+        firmware_file="$pure_file"
+        Update_Custom_Settings "local" "n"
+    fi
+else
+	# On subsequent runs, use the stored choice without prompting
+    if [ "$previous_choice" = "y" ]; then
+        firmware_file="$rog_file"
+    else
+        firmware_file="$pure_file"
+    fi
+fi
+
+# Flashing the chosen firmware
+Say "\033[32mFlashing $firmware_file...\033[0m"
+#hnd-write "$firmware_file"  # Execute the command to flash the firmware.
+
+# Wait for 3 minutes
+#sleep 60
+
+# Reboot the router
+#reboot
+    
+    read -p "Press Enter to continue..."
+}
+
+if [[ $1 == "run_now" ]]; then
+    # If the argument is "run_now", call the run_now function and exit
+    run_now
+    exit 0
+fi
 
 show_menu() {
   clear
@@ -390,7 +365,9 @@ while true; do
     2) change_build_type ;;
     3) change_schedule ;;
     e) exit ;;
-    *) echo "Invalid choice. Please try again." ;;
+    *) echo "Invalid choice. Please try again."
+   read -p "Press Enter to continue..."  # Pauses script until Enter is pressed
+   ;;
   esac
   else
   read -p "Enter your choice (1/2/e): " choice
@@ -398,7 +375,9 @@ while true; do
     1) run_now ;;
     2) change_schedule ;;
     e) exit ;;
-    *) echo "Invalid choice. Please try again." ;;
+    *) echo "Invalid choice. Please try again."
+   read -p "Press Enter to continue..."  # Pauses script until Enter is pressed
+   ;;
   esac
   fi
 done
