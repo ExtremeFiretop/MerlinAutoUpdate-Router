0d754df66534cff4d90271283ad3133898a69965
Joel Samson <joel.samson@hotmail.com>
2023-12-11 09:48:39 -0500

    Update MerlinAU.sh

--- MerlinAU.sh
+++ MerlinAU.sh
@@ -1049,7 +1049,7 @@ check_version_support() {
 
 check_model_support() {
     # List of unsupported models as a space-separated string
-    local unsupported_models="RT-AC68U RT-AC87U RT-AC56U RT-AC66U RT-AC3200 RT-N66U"
+    local unsupported_models="RT-AC68U RT-AC87U RT-AC56U RT-AC66U RT-AC3200 RT-N66U RT-AC66U_B1 RT-AC1900"
 
     # Get the current model
     local current_model="$(_GetRouterProductID_)"
