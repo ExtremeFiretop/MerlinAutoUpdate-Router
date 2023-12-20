56663302bce0b9fd284806a63b4faf4b47e1c367
Joel Samson <joel.samson@hotmail.com>
2023-12-20 00:29:14 -0500

    Update MerlinAU.sh

--- MerlinAU.sh
+++ MerlinAU.sh
@@ -1900,7 +1900,7 @@ Would you like to use the ROG build? (y/n)${NOct}\n"
         #----------------------------------------------------------#
         # In the rare case that the F/W Update gets "stuck" for
         # some reason & the "curl" cmd never returns, we create 
-        # a background child process that sleeps for 3 and a half minutes 
+        # a background child process that sleeps for 4 and a half minutes 
         # and then kills the "curl" process if it still exists. 
         # Otherwise, this child process does nothing & returns.        
         #----------------------------------------------------------#
