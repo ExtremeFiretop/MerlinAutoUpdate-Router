f19f31daf905114bfa0605a90bb51570c7116bef
Joel Samson <joel.samson@hotmail.com>
2024-03-20 21:58:06 -0400

    Update MerlinAU.sh

--- MerlinAU.sh
+++ MerlinAU.sh
@@ -487,7 +487,6 @@ _CheckForNewScriptUpdates_()
 {
 
    echo ""
-   Say "${GRNct}MerlinAU${NOct} v$SCRIPT_VERSION"
    local DLRepoVersionNum  ScriptVersionNum
 
    # Download the latest version file from the source repository
