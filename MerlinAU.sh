700ecd8731fbeb29ee2b07ad6955123e142be8f7
Joel Samson <joel.samson@hotmail.com>
2024-04-06 22:51:19 -0400

    Update MerlinAU.sh

--- MerlinAU.sh
+++ MerlinAU.sh
@@ -4,7 +4,7 @@
 #
 # Original Creation Date: 2023-Oct-01 by @ExtremeFiretop.
 # Official Co-Author: @Martinski W. - Date: 2023-Nov-01
-# Last Modified: 2024-April-02
+# Last Modified: 2024-Apr-02
 ###################################################################
 set -u
 
