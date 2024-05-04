b19c60871e9732a1c83edacb021636ff7b912535
ExtremeFiretop <joel.samson@hotmail.com>
2024-05-04 01:10:15 -0400

    Revert "Update MerlinAU.sh"
    This reverts commit 2dfb984d2801a6b01a3542810dd076d8e293a425.

--- MerlinAU.sh
+++ MerlinAU.sh
@@ -3136,17 +3136,16 @@ _CheckNodeFWUpdateNotification_()
    return 0
 }
 
-##------------------------------------------##
-## Modified by ExtremeFiretop [2024-May-03] ##
-##------------------------------------------##
+##----------------------------------------##
+## Modified by Martinski W. [2024-Mar-21] ##
+##----------------------------------------##
 _CheckTimeToUpdateFirmware_()
 {
    if [ $# -lt 2 ] || [ -z "$1" ] || [ -z "$2" ]
    then echo "**ERROR** **NO_PARAMS**" ; return 1 ; fi
 
-   local notifyTimeSecs postponeTimeSecs currentTimeSecs dstAdjustSecs dstAdjustDays
-   local fwNewUpdateNotificationDate fwNewUpdateNotificationVers fwNewUpdatePostponementDays
-   local currCronSchedule cronHour cronMinute nextCronRunSecs
+   local notifyTimeSecs  postponeTimeSecs  currentTimeSecs  dstAdjustSecs  dstAdjustDays
+   local fwNewUpdateNotificationDate  fwNewUpdateNotificationVers  fwNewUpdatePostponementDays
 
    _CheckNewUpdateFirmwareNotification_ "$1" "$2"
 
@@ -3165,36 +3164,33 @@ _CheckTimeToUpdateFirmware_()
 
    currentTimeSecs="$(date +%s)"
    notifyTimeStrn="$(echo "$fwNewUpdateNotificationDate" | sed 's/_/ /g')"
-   notifyTimeSecs="$(date -d "$notifyTimeStrn" +%s)"
+   notifyTimeSecs="$(date +%s -d "$notifyTimeStrn")"
 
-   # DST Adjustment Calculation
-   if [ "$(date +'%Z')" = "$(date -d "@$notifyTimeSecs" +'%Z')" ]
+   #----------------------------------------------------------------------
+   # Adjust calculation of postponed days as elapsed time in seconds to
+   # account for the hour discrepancy when Daylight Saving Time happens.
+   # This way we can avoid a scenario where the F/W Update "date+time"
+   # threshold is set one hour *after* the scheduled cron job is set
+   # to run again and check if it's time to update the router.
+   #----------------------------------------------------------------------
+   if [ "$(date -d @$currentTimeSecs +'%Z')" = "$(date -d @$notifyTimeSecs +'%Z')" ]
    then dstAdjustSecs=86400  #24-hour day is same as always#
    else dstAdjustSecs=82800  #23-hour day only when DST happens#
    fi
-
    dstAdjustDays="$((fwNewUpdatePostponementDays - 1))"
-   postponeTimeSecs="$(((dstAdjustDays * 86400) + dstAdjustSecs))"
-   upfwDateTimeSecs="$((notifyTimeSecs + postponeTimeSecs))"
-
-   # Retrieve current CRON schedule setting and calculate next run time
-   currCronSchedule="$(Get_Custom_Setting FW_New_Update_Cron_Job_Schedule)"
-   cronMinute=$(echo "$currCronSchedule" | cut -d ' ' -f 1)
-   cronHour=$(echo "$currCronSchedule" | cut -d ' ' -f 2)
-
-   # Calculate next cron run based on updated firmware date time
-   nextCronRunDate="$(date -d "@$upfwDateTimeSecs" +"%Y-%m-%d $cronHour:$cronMinute")"
-   nextCronRunSecs="$(date -d "$nextCronRunDate" +%s)"
-
-   # Check if calculated cron run time is before the postponed update time, add a day
-   if [ "$nextCronRunSecs" -lt "$upfwDateTimeSecs" ]; then
-       nextCronRunSecs="$(($nextCronRunSecs + 86400))"
+   if [ "$dstAdjustDays" -eq 0 ]
+   then postponeTimeSecs="$dstAdjustSecs"
+   else postponeTimeSecs="$(((dstAdjustDays * 86400) + dstAdjustSecs))"
    fi
 
-   upfwDateTimeStrn="$(date -d "@$nextCronRunSecs" +"%A, %Y-%b-%d %I:%M %p")"
+   if [ "$((currentTimeSecs - notifyTimeSecs))" -ge "$postponeTimeSecs" ]
+   then return 0 ; fi
+
+   upfwDateTimeSecs="$((notifyTimeSecs + postponeTimeSecs))"
+   upfwDateTimeStrn="$(date -d @$upfwDateTimeSecs +"%A, %Y-%b-%d %I:%M %p")"
 
    Say "The firmware update to ${GRNct}${2}${NOct} version is currently postponed for ${GRNct}${fwNewUpdatePostponementDays}${NOct} day(s)."
-   Say "The firmware update is expected to occur on ${GRNct}${upfwDateTimeStrn}${NOct}."
+   Say "The firmware update is expected to occur on or after ${GRNct}${upfwDateTimeStrn}${NOct}, depending on when your cron job is scheduled to check again."
    return 1
 }
 
