1a83d93e736b6457dea7903bf880d0b8f355e6c4
Joel Samson <joel.samson@hotmail.com>
2025-05-10 15:28:50 -0400

    Update MerlinAU.sh

--- MerlinAU.sh
+++ MerlinAU.sh
@@ -9,7 +9,7 @@
 set -u
 
 ## Set version for each Production Release ##
-readonly SCRIPT_VERSION=1.4.3
+readonly SCRIPT_VERSION=1.4.5
 readonly SCRIPT_NAME="MerlinAU"
 ## Set to "master" for Production Releases ##
 SCRIPT_BRANCH="dev"
