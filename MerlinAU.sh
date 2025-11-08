b129dc8a83fa5cdc9ef57bac6868fe5563091df8
Martinski4GitHub <119833648+Martinski4GitHub@users.noreply.github.com>
2025-11-07 18:48:00 -0800

    Updated for Next Release
    Updated for next Production Release.

--- MerlinAU.sh
+++ MerlinAU.sh
@@ -13,7 +13,7 @@ readonly SCRIPT_VERSION=1.5.6
 readonly SCRIPT_VERSTAG="25110520"
 readonly SCRIPT_NAME="MerlinAU"
 ## Set to "master" for Production Releases ##
-SCRIPT_BRANCH="master"
+SCRIPT_BRANCH="dev"
 
 ##----------------------------------------##
 ## Modified by Martinski W. [2024-Jul-03] ##
